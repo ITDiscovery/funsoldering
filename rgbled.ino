8e3766db375ba5a7854bc050657d789c19f9abf9
Peter Nichols <raspbian@itdiscovery.info>
2023-09-23 13:44:32 -0500

    Update rgbled.ino
    Fixed Blinky and logic errors

--- rgbled.ino
+++ rgbled.ino
@@ -1,20 +1,98 @@
+#include <EEPROM.h>
+
 #define red 0 //PIN 5
 #define blue 1 //PIN 6
-#define green 2 //PIN 7
-#define buttonPin 4 //PIN 3
+#define green 2 //PIN 7 - Not a PWM Pin
+#define buttonPin 3 //PIN 3
+
+//Version 1 Board Pinout
+//#define red 0 //PIN 5
+//#define blue 4 //PIN 3
+//#define green 1 //PIN 6
+//#define buttonPin 3 //PIN 2
+
 unsigned int colorselect = 0;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
-unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+unsigned long debounceDelay = 20;    // the debounce time; increase if the output flickers
 int lastButtonState = LOW;  // the previous reading from the input pin
 int buttonState;            // the current reading from the input pin
 
 void setup() 
 {
-  pinMode(red,OUTPUT);
   pinMode(green,OUTPUT);
-  pinMode(blue,OUTPUT);
   pinMode(buttonPin,INPUT_PULLUP);
+  colorselect = EEPROM.read(0);
+  ledlight(colorselect);
 }
+
+void ledlight(byte ledcolor) {
+          switch (ledcolor) {
+          case 0:
+              //Off
+              analogWrite(red,0);
+              digitalWrite(green,LOW);
+              analogWrite(blue,0);
+              EEPROM.write(0, 0);
+              break;
+          case 1:
+              //Red
+              analogWrite(red,255);
+              digitalWrite(green,LOW);
+              analogWrite(blue,0);
+              EEPROM.write(0, 1);
+              break;
+          case 2:
+              //Green
+              analogWrite(red,0);
+              digitalWrite(green,255);
+              analogWrite(blue,0);
+              EEPROM.write(0, 2);
+              break;
+          case 3:
+              //Blue
+              analogWrite(red,0);
+              digitalWrite(green,LOW);
+              analogWrite(blue,255);
+              EEPROM.write(0, 3);
+              break;
+          case 4:
+              //Aqua
+              analogWrite(red,0);
+              digitalWrite(green,HIGH);
+              analogWrite(blue,200);
+              EEPROM.write(0, 4);
+              break;
+          case 5:
+              //Purple
+              analogWrite(red,255);
+              digitalWrite(green,LOW);
+              analogWrite(blue,200);
+              EEPROM.write(0, 5);
+              break;
+          case 6:
+              //Yellow
+              analogWrite(red,255);
+              digitalWrite(green,HIGH);
+              analogWrite(blue,0);
+              EEPROM.write(0, 6);
+              break;
+          case 7:
+              //Bright White
+              analogWrite(red,255);
+              digitalWrite(green,HIGH);
+              analogWrite(blue,255);
+              EEPROM.write(0, 7);
+              break;
+          default:
+              colorselect=0;
+              analogWrite(red,0);
+              analogWrite(green,0);
+              analogWrite(blue,0);
+              EEPROM.write(0, 0);
+            }
+}
+
+
 void loop() 
 {
 
@@ -34,9 +112,10 @@ void loop()
     if (reading != buttonState) {
       buttonState = reading;
 
-      // only change to the next color if the new button state is HIGH
-      if (buttonState == HIGH) {
-        colorselect++;
+      // only change to the next color if the new button state is LOWx
+      if (buttonState == LOW) {
+          colorselect++;
+          ledlight(colorselect);
       }
     }
   }
@@ -44,65 +123,4 @@ void loop()
   // save the reading. Next time through the loop, it'll be the lastButtonState:
   lastButtonState = reading;
 
-  switch (colorselect) {
-    case 0:
-       //Off
-       analogWrite(red,0);
-       analogWrite(green,0);
-       analogWrite(blue,0);
-       break;
-    case 1:
-       //Red
-       analogWrite(red,255);
-       analogWrite(green,0);
-       analogWrite(blue,0);
-       break;
-    case 2:
-       //Green
-       analogWrite(red,0);
-       analogWrite(green,255);
-       analogWrite(blue,0);
-       break;
-    case 3:
-       //Blue
-       analogWrite(red,0);
-       analogWrite(green,0);
-       analogWrite(blue,255);
-       break;
-    case 4:
-       //Aqua
-       analogWrite(red,0);
-       analogWrite(green,255);
-       analogWrite(blue,255);
-       break;
-    case 5:
-       //Soft White
-       analogWrite(red,64);
-       analogWrite(green,64);
-       analogWrite(blue,64);
-       break;
-    case 6:
-       //Med White
-       analogWrite(red,128);
-       analogWrite(green,128);
-       analogWrite(blue,128);
-       break;
-    case 7:
-       //Frim White
-       analogWrite(red,192);
-       analogWrite(green,192);
-       analogWrite(blue,192);
-       break;
-    case 8:
-       //Bright White
-       analogWrite(red,255);
-       analogWrite(green,255);
-       analogWrite(blue,255);
-       break;
-    case 9:
-       //Dance Mode
-       
-    default:
-       colorselect=0;
-       }
 }
